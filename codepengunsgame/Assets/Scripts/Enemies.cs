c4a938c49522d2284bcfd0a13bd5452cfe15c4f9
Blue <bluepilkintonching@gmail.com>
2022-07-23 14:48:28 +1200

    Optimized the enemy script

--- codepengunsgame/Assets/Scripts/Enemies.cs
+++ codepengunsgame/Assets/Scripts/Enemies.cs
@@ -4,108 +4,54 @@ using UnityEngine;
 
 public class Enemies : MonoBehaviour
 {
-    [SerializeField]
-    private bool seenPlayer;
-    [SerializeField]
+    public GameObject bullet;
+    public GameObject player;
+    public Rigidbody2D rb;
+    public float maxRange;
+    public float minRange;
+
+    private float currentBulletDelay;
+    private float bulletDelay = 0.5f;
+
     private bool shotBullet = false;
-    [SerializeField]
-    private GameObject bullet;
 
-    [SerializeField]
-    private float currentCDTimer;
-    [SerializeField]
-    private float setCDTimer = 0.5f;
+    public float moveSpeed;
 
-    [SerializeField]
-    private float moveSpeed;
-
-    [SerializeField]
-    private bool stopMoving;
-
-    [SerializeField]
     private float distFromPlayer;
-
-    Transform target;
-    Vector2 moveDir;
-
-    [SerializeField]
-    private GameObject player;
-
-    Rigidbody2D rb;
-
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        target = GameObject.Find("Player").transform;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        distFromPlayer = Vector2.Distance(this.transform.position, player.transform.position);
-
-        if(target && seenPlayer == true)
-        {
-            Vector3 dir = (target.position - transform.position).normalized;
-            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-            rb.rotation = angle;
-            moveDir = dir;
-        }
-
-        if(shotBullet == true)
-        {
-            currentCDTimer = Mathf.Clamp(currentCDTimer -= Time.deltaTime, 0f, setCDTimer);
-        }
-
-        if(currentCDTimer <= 0f)
-        {
-            shotBullet = false;
-        }
-
-        if (seenPlayer == true)
-        {
-            rb.constraints = RigidbodyConstraints2D.None;
-            Shoot();
-        }
-        else if(seenPlayer == false)
-        {
-            rb.constraints = RigidbodyConstraints2D.FreezePosition;
-        }
-
-        if(distFromPlayer >= 15f)
-        {
-            seenPlayer = false;
-        }
-        else if (distFromPlayer <= 15f)
-        {
-            seenPlayer = true;
-        }
-
-        if(distFromPlayer <= 5f)
-        {
-            seenPlayer = false;
-        }
-    }
 
     private void FixedUpdate()
     {
-        if(target && seenPlayer == true)
+        distFromPlayer = Vector2.Distance(transform.position, player.transform.position);
+
+        if (distFromPlayer <= maxRange)
         {
-            rb.velocity = new Vector2(moveDir.x, moveDir.y) * moveSpeed;
+            Vector3 dir = (player.transform.position - transform.position).normalized;
+            Vector3 angle = new Vector3(0, 0, Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg);
+            rb.rotation = angle.z;
+            if (distFromPlayer >= minRange)
+            {
+                rb.velocity = new Vector2(dir.x, dir.y) * moveSpeed;
+            }
+            Shoot();
+        }
+
+        if (shotBullet == true)
+        {
+            currentBulletDelay = Mathf.Clamp(currentBulletDelay -= Time.deltaTime, 0f, bulletDelay);
+        }
+
+        if (currentBulletDelay <= 0f)
+        {
+            shotBullet = false;
         }
     }
 
     private void Shoot()
     {
-        if(currentCDTimer <= 0f && shotBullet == false)
+        if(currentBulletDelay <= 0f && shotBullet == false)
         {
             Instantiate(bullet, this.gameObject.transform.position, this.gameObject.transform.rotation);
-            currentCDTimer = setCDTimer;
+            currentBulletDelay = bulletDelay;
             shotBullet = true;
         }
     }
